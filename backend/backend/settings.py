d9bbf417e82d64d2dc2786be0fb53c8fd0475b5b
Rui Fang <1016799355@qq.com>
2026-01-24 20:00:02 +0800

    创建导航栏

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -9,10 +9,10 @@ https://docs.djangoproject.com/en/6.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
-# import mimetypes
-# mimetypes.add_type("application/javascript", ".js", True)
-# mimetypes.add_type("application/javascript", ".mjs", True)
-# mimetypes.add_type("text/css", ".css", True)
+import mimetypes
+mimetypes.add_type("application/javascript", ".js", True)
+mimetypes.add_type("application/javascript", ".mjs", True)
+mimetypes.add_type("text/css", ".css", True)
 from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
