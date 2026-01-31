c815c55baa1b4a485300fc9c4f8f0af293473492
Rui Fang <1016799355@qq.com>
2026-01-31 11:02:14 +0800

    创建页面实现路由

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,10 +1,62 @@
+import CreateIndex from '@/views/create/CreateIndex.vue'
+import NotFoundIndex from '@/views/error/NotFoundIndex.vue'
+import FrendIndex from '@/views/friend/FrendIndex.vue'
+import HomepageIndex from '@/views/homepage/HomepageIndex.vue'
+import LoginIndex from '@/views/user/account/LoginIndex.vue'
+import RegisterIndex from '@/views/user/account/RegisterIndex.vue'
+import ProfileIndex from '@/views/user/profile/ProfileIndex.vue'
+import SpaceIndex from '@/views/user/space/SpaceIndex.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-    
+    {
+      path: '/',
+      component: HomepageIndex,
+      name: 'homepage-index'
+    },
+    {
+      path: '/friend/',
+      component: FrendIndex,
+      name: 'friend-index'
+    },
+    {
+      path: '/create/',
+      component: CreateIndex,
+      name: 'create-index'
+    },
+    {
+      path: '/404/',
+      component: NotFoundIndex,
+      name: '404'
+    },
+    {
+      path: '/user/account/login/',
+      component: LoginIndex,
+      name: 'user-account-login-index'
+    },
+    {
+      path: '/user/account/register/',
+      component: RegisterIndex,
+      name: 'user-account-register-index'
+    },
+    {
+      path: '/user/space/:user_id/',
+      component: SpaceIndex,
+      name: 'user-space-index'
+    },
+    {
+      path: '/user/Profile/',
+      component: ProfileIndex,
+      name: 'user-profile-index'
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      component: NotFoundIndex,
+      name: 'not-found-index'
+    },
   ],
 })
 
