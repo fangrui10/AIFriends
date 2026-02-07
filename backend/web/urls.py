51bbcaf6f6bd9a01e6640c22aa79ab26826e0590
Rui Fang <1016799355@qq.com>
2026-02-07 18:57:09 +0800

    实现角色增删改查后端api

--- backend/web/urls.py
+++ backend/web/urls.py
@@ -4,6 +4,10 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
 )
 
+from web.views.create.character.create import CreateCharacterView
+from web.views.create.character.get_single import GetSingleCharacterView
+from web.views.create.character.remove import RemoveCharacterView
+from web.views.create.character.update import UpdateCharacterView
 from web.views.index import index
 from web.views.user.account.get_user_info import GetUserInfoView
 from web.views.user.account.login import LoginView
@@ -21,6 +25,10 @@ urlpatterns = [
     path('api/user/account/refresh_token/', RefreshTokenView.as_view()),
     path('api/user/account/get_user_info/', GetUserInfoView.as_view()),
     path('api/user/profile/update/', UpdateProfileView.as_view()),
+    path('api/create/character/create/', CreateCharacterView.as_view()),
+    path('api/create/character/get_single/', GetSingleCharacterView.as_view()),
+    path('api/create/character/update/', UpdateCharacterView.as_view()),
+    path('api/create/character/remove/', RemoveCharacterView.as_view()),
     path('', index),
 
     re_path(r'^(?!media/|static/|assets/).*$', index), # 其他路径都交给前端路由处理
---
+++ backend/web/views/create/character/get_single.py
@@ -0,0 +1,29 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+from web.models.character import Character
+
+
+class GetSingleCharacterView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        try:
+            character_id = request.query_params['character_id']
+            character = Character.objects.get(id=character_id, author__user=request.user) # __获取关联对象的属性
+
+            return Response({
+                'result': 'success',
+                'character': {
+                    'id': character.id,
+                    'name': character.name,
+                    'profile': character.profile,
+                    'photo': character.photo.url,
+                    'background_image': character.background_image.url,
+                }
+            })
+        except:
+            return Response({
+                'result': '系统异常，请稍后重试'
+            })
