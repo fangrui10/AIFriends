b5849aed05afee63fb673d59a7bee804f83d0e98
Rui Fang <1016799355@qq.com>
2026-02-07 18:13:58 +0800

    实现编辑资料页面前端

---
+++ frontend/src/views/user/profile/components/Profile.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref, watch } from 'vue';
+const props = defineProps(['profile'])
+const myProfile = ref(props.profile)
+
+
+watch(() => props.profile, newVal => {
+    myProfile.value = newVal
+})
+
+defineExpose({
+    myProfile
+})
+</script>
+
+<template>
+    <fieldset class="fieldset">
+        <label class="label text-base">
+            简介
+        </label>
+        <textarea v-model="myProfile" class="textarea w-108" rows="6" placeholder="简介"></textarea>
+    </fieldset>
+
+</template>
+
+<style scoped>
+
+</style>
