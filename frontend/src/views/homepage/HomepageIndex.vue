0d662f699bf7338b3fbfc03c045a7ea3b981f932
Rui Fang <1016799355@qq.com>
2026-02-13 16:35:30 +0800

    实现首页前后端

--- frontend/src/views/homepage/HomepageIndex.vue
+++ frontend/src/views/homepage/HomepageIndex.vue
@@ -1,13 +1,92 @@
-<template>
-    <div>
-        主页
-    </div>
-</template>
-
 <script setup>
+import Character from '@/components/character/Character.vue';
+import api from '@/js/http/api';
+import { ref, useTemplateRef, nextTick, onMounted, onBeforeMount } from 'vue';
 
+const characters = ref([])
+const isLoading = ref(false)
+const hasCharacters = ref(true)
+const sentinelRef = useTemplateRef('sentinel-ref')
+
+
+function checkSentinelVisible() {  // 判断哨兵是否能被看到
+  if (!sentinelRef.value) return false
+
+  const rect = sentinelRef.value.getBoundingClientRect()
+  return rect.top < window.innerHeight && rect.bottom > 0
+}
+
+
+async function loadMore() {
+    if (isLoading.value || !hasCharacters.value) return
+
+    isLoading.value = true
+
+    let newCharacters = []
+    try {
+        const res = await api.get('/api/homepage/index/', {
+            params: {
+                items_count: characters.value.length,
+            }
+        })
+        const  data = res.data
+        if (data.result === 'success') {
+            newCharacters = data.characters
+        }
+    } catch (error) {
+    } finally {
+        isLoading.value = false
+
+        if (newCharacters.length === 0) {
+            hasCharacters.value = false
+        } else {
+            characters.value.push(...newCharacters)
+            await nextTick()
+
+            if (checkSentinelVisible()) {
+                loadMore()
+            }
+        }
+    }
+}
+
+let observer = null
+onMounted(async () => {
+    await loadMore()
+
+    observer = new IntersectionObserver(
+        entries => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting){
+                    loadMore()
+                }
+            })
+        },
+        {
+            root: null,
+            rootMargin: '2px',
+            threshold: 0
+        }
+    )
+    observer.observe(sentinelRef.value)
+})
+
+onBeforeMount(() => {
+    observer?.disconnect()    
+})
 </script>
 
-<style lang="scss" scoped>
+<template>
+    <div class="flex flex-col items-center mb-12">
+        <div class="grid grid-cols-[repeat(auto-fill,minmax(240px,1fr))] gap-9 mt-12 justify-items-center w-full px-9">
+            <Character v-for="character in characters" :key="character.id" :character="character" />
+        </div>
 
-</style>
+        <div ref="sentinel-ref" class="h-2 mt-8"> </div>
+        <div v-if="isLoading" class="text-gray-500 mt-4">加载中。。。</div>
+        <div v-if="!hasCharacters" class="text-gray-500 mt-4">没有更多角色了</div>
+    </div>
+    
+</template>
+
+<style scoped></style>
