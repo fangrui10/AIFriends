5e413479c270e09fc37587db44587f03e3b21eb3
Rui Fang <1016799355@qq.com>
2026-02-13 15:32:28 +0800

    实现角色卡片

--- frontend/src/views/user/space/SpaceIndex.vue
+++ frontend/src/views/user/space/SpaceIndex.vue
@@ -3,6 +3,7 @@ import { nextTick, onBeforeUnmount, onMounted, ref, useTemplateRef } from 'vue';
 import UserInfoField from './components/UserInfoField.vue';
 import api from '@/js/http/api';
 import { useRoute } from 'vue-router';
+import Character from '@/components/character/Character.vue';
 
 const userProfile = ref(null)
 const characters = ref([])
@@ -35,12 +36,9 @@ async function loadMore() {
         if (data.result === 'success') {
             userProfile.value = data.user_profile
             newCharacters = data.characters
-        } else {
-            console.error('加载角色列表失败:', data.message)
         }
 
     } catch (error) {
-        console.error('加载角色列表失败:', error)
     } finally {
         isLoading.value = false
         if (newCharacters.length === 0) {
@@ -79,6 +77,12 @@ onMounted( async () => {
     observer.observe(sentinelRef.value)
 })
 
+
+function removeCharacter(characterId) {
+    characters.value = characters.value.filter(c => c.id !== characterId)
+}
+
+
 onBeforeUnmount(() => {
     observer?.disconnect()
 })
@@ -88,8 +92,15 @@ onBeforeUnmount(() => {
     <div class="flex flex-col items-center mb-12">
         <UserInfoField :userProfile="userProfile"/>
         <div class="grid grid-cols-[repeat(auto-fill,minmax(240px,1fr))] gap-9 mt-12 justify-items-center w-full px-9">
+            <Character 
+                v-for="character in characters"
+                :key="character.id"
+                :character="character"
+                :canEdit="true"
+                @remove="removeCharacter"
+            />
         </div>
-        <div ref="sentinel-ref" class="h-2 mt-8 w-100 bg-red-500"></div>
+        <div ref="sentinel-ref" class="h-2 mt-8"></div>
         <div v-if="isLoading" class="text-gray-500 mt-4">加载中。。。</div>
         <div v-if="!hasCharacters" class="text-gray-500 mt-4">没有更多角色了</div>
 
